8f6b916bfd68dba6c7531cf710d92fc4a11f0439
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-25 19:02:23 +0100

    fix endpoint in frontend

--- frontend/js/api-config.js
+++ frontend/js/api-config.js
@@ -1,10 +1,10 @@
 // api-config.js
 export const API_ENDPOINTS = {
-    GET_ALL_LEVELS: "https://rubiks-server.onrender.com//get_all_levels/",
-    GET_LEVEL: "https://rubiks-server.onrender.com//get_level/",
-    CREATE_LEVEL: "https://rubiks-server.onrender.com//create_level/",
-    DELETE_LEVEL: "https://rubiks-server.onrender.com//delete_level/",
-    UPDATE_LEVEL: "https://rubiks-server.onrender.com//update_level/"
+    GET_ALL_LEVELS: "https://rubiks-server.onrender.com/get_all_levels/",
+    GET_LEVEL: "https://rubiks-server.onrender.com/get_level/",
+    CREATE_LEVEL: "https://rubiks-server.onrender.com/create_level/",
+    DELETE_LEVEL: "https://rubiks-server.onrender.com/delete_level/",
+    UPDATE_LEVEL: "https://rubiks-server.onrender.com/update_level/"
 };
 
 export const PAGE_ROUTES = {
