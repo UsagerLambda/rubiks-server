c90420b44bbae16eec4df6daa65a43b5bd97fb3c
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-25 18:56:24 +0100

    Deploy frontend

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restx import Api
 from database import init_db
 from flask_cors import CORS
+import os
 
 
 from endpoint.create_level import api as create_api
@@ -11,7 +12,7 @@ from endpoint.get_level import api as get_api
 from endpoint.get_all_levels import api as get_all_api
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend')
 CORS(app)
 
 
@@ -31,13 +32,19 @@ api.add_namespace(get_api)
 api.add_namespace(get_all_api)
 
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+   if path != "" and os.path.exists(os.path.join('frontend', path)):
+       return send_from_directory('frontend', path)
+   else:
+       return send_from_directory('frontend', 'index.html')
+
+
 if __name__ == '__main__':
-    # Create database tables
-    with app.app_context():
-        db.create_all()
+   # Create database tables
+   with app.app_context():
+       db.create_all()
 
 
-    app.run(host='0.0.0.0', port=5123)
-
-
-
+   app.run(host='0.0.0.0', port=5123)
